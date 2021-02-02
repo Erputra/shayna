43e6f838e4328ac8b4e8e4bcff7fa43fedc91e06
unknown <enricoriski49@gmail.com>
2021-02-02 21:42:39 +0700

    upload projek vue

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/product',
+        name: 'Product',
+        component: () =>
+            import ( /* webpackChunkName: "product" */ '../views/Product.vue')
+    },
+    {
+        path: '/shoppingcart',
+        name: 'ShoppingCart',
+        component: () =>
+            import ( /* webpackChunkName: "shoppingcart" */ '../views/ShoppingCart.vue')
+    },
+    {
+        path: '/success',
+        name: 'Success',
+        component: () =>
+            import ( /* webpackChunkName: "success" */ '../views/Success.vue')
+    }
+
+
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <WomenBanner/>
+    <Instagram/>
+    <LogosPartner/>
+    <RelatedProduct/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import Hero from "../components/Hero";
+import WomenBanner from "../components/WomenBanner"
+import Instagram from "../components/Instagram"
+import LogosPartner from "../components/Logos"
+import Footer from "../components/Footer"
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    WomenBanner,
+    Instagram,
+    LogosPartner,
+    Footer,
+    },
+};
+</script>
